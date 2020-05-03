9d6de756192abe71f45b43290fbf3aaccfb93abd
nickdn <nickdndev@gmail.com>
2020-05-03 13:45:25 +0300

    init commit

---
+++ src/main/scala/com/news4you/Layers.scala
@@ -0,0 +1,37 @@
+package com.news4you
+
+import com.news4you.config.{AppConfigProvider, ConfigProvider, DbConfigProvider}
+import com.news4you.repository.{DoobieTodoRepository, TodoRepository}
+import zio.ZLayer
+import zio.blocking.Blocking
+import zio.logging.Logging.Logging
+import zio.logging.slf4j.Slf4jLogger
+
+object Layers {
+
+  type Layer0Env =
+    ConfigProvider with Logging with Blocking
+
+  type Layer1Env =
+    Layer0Env with AppConfigProvider with DbConfigProvider
+
+  type Layer2Env =
+    Layer1Env with TodoRepository
+
+  type AppEnv = Layer2Env
+
+  object live {
+
+    val layer0: ZLayer[Blocking, Throwable, Layer0Env] =
+      Blocking.any ++ ConfigProvider.live ++ Slf4jLogger.make((_, msg) => msg)
+
+    val layer1: ZLayer[Layer0Env, Throwable, Layer1Env] =
+      AppConfigProvider.fromConfig ++ DbConfigProvider.fromConfig ++ ZLayer.identity
+
+    val layer2: ZLayer[Layer1Env, Throwable, Layer2Env] =
+      DoobieTodoRepository.layer ++ ZLayer.identity
+
+    val appLayer: ZLayer[Blocking, Throwable, AppEnv] =
+      layer0 >>> layer1 >>> layer2
+  }
+}
